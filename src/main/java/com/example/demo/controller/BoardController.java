7ceb97cb63f1def44f720ae255938c38a922bddc
danhyeon <sodanhyun@gmail.com>
2023-07-26 11:47:49 +0900

    7/26 실습 교육
    사이드바 및 헤더 레이아웃 및 요소 추가
    게시판 페이지 생성 및 컨트롤러 매핑
    게시판 테이블 생성
    게시판 엔티티 설계

---
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/board")
+public class BoardController {
+    @GetMapping(value = "/info")
+    public String boardInfo() {
+        return "/boards/boardInfo";
+    }
+}
