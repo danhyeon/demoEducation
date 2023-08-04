b64da9258b1db2fd265a965535deb90d26c1fc44
danhyeon <sodanhyun@gmail.com>
2023-08-04 12:32:31 +0900

    8/4 실습

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable();
+
+        httpSecurity.formLogin()
+                .loginPage("/member/login")
+                .defaultSuccessUrl("/")
+                .usernameParameter("email")
+//                .failureHandler(loginFailHandler())
+//                .failureUrl()
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+                .deleteCookies("JSESSIONID")
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutSuccessUrl("/member/login")
+                ;
+
+        httpSecurity.authorizeRequests()
+                .mvcMatchers("/").hasAnyRole("USER", "ADMIN")
+                .anyRequest().permitAll()
+                ;
+
+        return httpSecurity.build();
+    }
+
+}
